36deed15fe47f159c526767a47803a6ca2aad813
kaleayush <ayushkale85.33@gmail.com>
2024-02-06 19:10:19 +0530

    ThemeChanger Application

---
+++ 09Themeswitcher/src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { ThemeProvider } from "./Context/theme";
+import ThemeBtn from "./Components/ThemeBtn";
+import Card from "./Components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  useEffect(() => {
+    const dom = document.querySelector("html").classList;
+    dom.remove("light", "dark");
+    dom.add(themeMode);
+  }, [themeMode]);
+  return (
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+           <ThemeBtn/>
+          </div>
+            <Card/>
+          <div className="w-full max-w-sm mx-auto">{/* card */}</div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
